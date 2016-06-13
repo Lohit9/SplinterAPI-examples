7fe31f676536b4ba55f08cc2bdbeb427c366b7dc
Lohit Talasila <tlohit@hotmail.com>
2016-06-12 21:54:15 -0400

    modified example-2 to accept screenshot URL as commandline paramter

--- example2.py
+++ example2.py
@@ -1,12 +1,18 @@
+import sys
 # Quick Screenshot - Just mention the URL and take a screenshot
 
 from splinter import Browser
 
 browser = Browser('chrome')
 
-url = "http://lohittalasila.com"
+#url = "http://lohittalasila.com"
+url = str(sys.argv[1])
+print "screenshot of the page found at "+url+" has been taken you can access it by using the command - open screenshot.png"
+
+print "Thanks for using quick screenshot"
+
 browser.visit(url)
 
 browser.driver.save_screenshot('screenshot.png')
 
-browser.quit()
+browser.quit() 
